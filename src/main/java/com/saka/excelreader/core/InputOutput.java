cd793c43ba83c4f3774800b0b2fc630be3959028
someone <someone@someplace.com>
2019-06-03 14:56:46 +0530

    modified code to read from json also created new files

---
+++ src/main/java/com/saka/excelreader/core/InputOutput.java
@@ -0,0 +1,71 @@
+package com.saka.excelreader.core;
+
+public class InputOutput {
+	
+	Input input;
+	Input output;
+		
+	public InputOutput()
+	{
+	}
+	public Input getInput() {
+		return input;
+	}
+
+	public void setInput(Input input) {
+		this.input = input;
+	}
+
+	public Input getOutput() {
+		return output;
+	}
+
+	public void setOutput(Input output) {
+		this.output = output;
+	}
+
+	@Override
+	public String toString() {
+		return "InputOutput [input=" + input + ", output=" + output + "]";
+	}
+
+	
+	
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
--- src/main/java/com/saka/excelreader/core/ReadWrite.java
+++ src/main/java/com/saka/excelreader/core/ReadWrite.java
@@ -2,39 +2,41 @@ package com.saka.excelreader.core;
 
 import java.io.File;
 import java.io.FileInputStream;
-
 import java.io.IOException;
-import java.util.Iterator;
-
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 
 @Component
 public class ReadWrite {
-	
+	/*
 	public void getData(){
 		//ClassLoader classLoader = new ReadResourceFileDemo().getClass().getClassLoader();
 		File  file = new File(getClass().getClassLoader().getResource("Test.xlsx").getFile());
 		try {
-			FileInputStream  fis = new FileInputStream(file);
+			 FileInputStream  fis = new FileInputStream(file);
 			 XSSFWorkbook workbook = new XSSFWorkbook(fis);
 
-	            XSSFSheet sheet = workbook.getSheetAt(0);
-	            
+	           // XSSFSheet sheet = workbook.getSheetAt(0);
+			  XSSFSheet sheet = workbook.getSheet("sheet1");
 	            FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
 	        //   workbook.getCreationHelper().createFormulaEvaluator().evaluateAll();
 	           
 	            XSSFFormulaEvaluator.evaluateAllFormulaCells(workbook);
 	            
 	            Cell cell1 = sheet.createRow(2).createCell(1);
+	          //  Cell cell1 = sheet.createRow(
 	            cell1.setCellValue(1);
 	            Cell cell2 = sheet.createRow(3).createCell(1);
 	            cell2.setCellValue(2);
@@ -78,24 +80,55 @@ public class ReadWrite {
             System.out.println(evaluator.evaluate(sheet2.getRow(1).getCell(1)).getNumberValue());
             System.out.println(evaluator.evaluate(sheet2.getRow(2).getCell(1)).getNumberValue());
             
-            System.out.println(evaluator.evaluate(sheet2.getRow(3).getCell(1)).getNumberValue());
-            
-	            
+            System.out.println(evaluator.evaluate(sheet2.getRow(3).getCell(1)).getNumberValue());            
 		} catch (  IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}	
+	}*/
+	
+	    public void readJSON() throws IOException
+	    {
+			//read json file data to String byte[] jsonData =
+	    	//byte[] jsonData = Files.readAllBytes(Paths.get("D:\\internship\\ExcelReader\\src\\main\\resources\\fields.json"));
+		 
+	    	byte[] jsonData = Files.readAllBytes(new File(getClass().getClassLoader().getResource("fields.json").getFile()).toPath());
+	    	
+	    	//create ObjectMapper instance
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			//convert json string to object
+			InputOutput io = objectMapper.readValue(jsonData, InputOutput.class);
+			
+			System.out.println("Inputoutput Object\n"+io);
+			
+			// Copy output test
+			copyValuesToOutput(io);
+			
+			//configure Object mapper for pretty print
+			objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+			
 		}
+
 		
-		
-	}
+	  private void copyValuesToOutput(InputOutput io) { 
+		  // TODO Auto-generated method stub 
+	  System.out.println("===hello java-=====");
+	  System.out.println("Output is - " + io.getOutput());
+	  io.setOutput(io.getInput()); 
+	  }
+	 
 	
-	
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ReadWrite rw = new ReadWrite();
-		
-		rw.getData();
+		rw.readJSON();
 		
 	}
+	/*public static void main(String[] args) {
+	ReadWrite rw = new ReadWrite();
+
+	//rw.getData();		
+}*/
+
 
 }
